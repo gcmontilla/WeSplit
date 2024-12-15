d869b27a530dbf1ed5f16e5e406d4893a0372f1c
Greg <greg@Judys-Mac-mini.local>
2024-12-14 22:22:33 -0800

    init commit

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Greg on 12/14/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
